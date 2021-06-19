8179d06aec9d125d1dc1b97d92d60eb6832a98b0
kscheltema <k87scheltema@outlook.com>
2021-06-19 16:35:43 +0200

    most basic redux file

--- redux-demo.js
+++ redux-demo.js
@@ -1,1 +1,19 @@
 const redux = require("redux");
+
+const counterReducer = (state = { counter: 0 }, action) => {
+  return {
+    counter: state.counter + 1,
+  };
+}; //no side effects within this function should be pure
+
+const store = redux.createStore(counterReducer);
+console.log(store.getState()); //initial state
+
+const counterSubscriber = () => {
+  const latestState = store.getState();
+  console.log(store.getState()); //latest updated state
+};
+
+store.subscribe(counterSubscriber);
+
+store.dispatch({ type: "increment" });
